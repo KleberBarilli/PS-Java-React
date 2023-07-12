87f24663f5e02e0f7dfd31aa6f1ecb66e939cd78
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-12 13:16:50 -0300

    criando query para buscar transferencias por intervalo de datas

--- src/main/java/br/com/banco/infra/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/infra/repositories/TransferenciaRepository.java
@@ -1,9 +1,16 @@
 package br.com.banco.infra.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.banco.domain.entities.Transferencia;
 
+import java.time.LocalDate;
+
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
 
+    @Query("SELECT t FROM Transferencia t WHERE t.dataTransferencia >= :dataInicial AND t.dataTransferencia < :dataFinal")
+    List<Transferencia> buscarPorIntervaloDeData(LocalDate dataInicial, LocalDate dataFinal);
 }
