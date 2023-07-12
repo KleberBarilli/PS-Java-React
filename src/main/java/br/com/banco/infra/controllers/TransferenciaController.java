5d15702c11d309cf116d6c9a776bbe766caed8f2
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-12 16:29:12 -0300

    recebendo pageNumber e pageSize via query param

--- src/main/java/br/com/banco/infra/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/infra/controllers/TransferenciaController.java
@@ -1,7 +1,6 @@
 package br.com.banco.infra.controllers;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.banco.domain.entities.Transferencia;
+import br.com.banco.constants.AppConstants;
+import br.com.banco.domain.responses.TransferenciaResponse;
 import br.com.banco.domain.services.TransferenciaService;
 import lombok.RequiredArgsConstructor;
 
@@ -22,17 +22,20 @@ public class TransferenciaController {
     private final TransferenciaService service;
 
     @GetMapping
-    public ResponseEntity<List<Transferencia>> listar(
+    public ResponseEntity<TransferenciaResponse> listar(
             @RequestParam(value = "dataInicial", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicial,
             @RequestParam(value = "dataFinal", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFinal,
-            @RequestParam(value = "nomeOperador", required = false) String nomeOperador) {
+            @RequestParam(value = "nomeOperador", required = false) String nomeOperador,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNumber) {
 
         if (dataInicial != null && dataFinal != null && dataFinal.isBefore(dataInicial)) {
             throw new IllegalArgumentException("A data final não pode ser menor que a data inicial");
 
         }
 
-        List<Transferencia> transferencias = service.obterTransferencias(dataInicial, dataFinal, nomeOperador);
+        TransferenciaResponse transferencias = service.obterTransferencias(dataInicial, dataFinal, nomeOperador,
+                pageNumber, pageSize);
 
         return ResponseEntity.ok(transferencias);
     }
