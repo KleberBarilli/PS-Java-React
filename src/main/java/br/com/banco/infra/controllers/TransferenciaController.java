682c336622a0d556488e3b4e0dc42bfca1786eea
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-12 12:54:08 -0300

    adicionando queryParams no controller

--- src/main/java/br/com/banco/infra/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/infra/controllers/TransferenciaController.java
@@ -1,10 +1,13 @@
 package br.com.banco.infra.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.domain.entities.Transferencia;
@@ -19,9 +22,17 @@ public class TransferenciaController {
     private final TransferenciaService service;
 
     @GetMapping
-    public ResponseEntity<List<Transferencia>> listar() {
+    public ResponseEntity<List<Transferencia>> listar(
+            @RequestParam(value = "dataInicial", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicial,
+            @RequestParam(value = "dataFinal", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFinal,
+            @RequestParam(value = "nomeOperador", required = false) String nomeOperador) {
 
-        List<Transferencia> transferencias = service.findAll();
+        if (dataInicial != null && dataFinal != null && dataFinal.isBefore(dataInicial)) {
+            throw new IllegalArgumentException("A data final não pode ser menor que a data inicial");
+
+        }
+
+        List<Transferencia> transferencias = service.obterTransferencias(dataInicial, dataFinal, nomeOperador);
 
         return ResponseEntity.ok(transferencias);
     }
