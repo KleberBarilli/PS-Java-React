b3e4fd551d955075cc4c00d05ceaeffe208f64f0
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-12 16:28:05 -0300

    criando padrao de response para transferencia com paginacao

---
+++ src/main/java/br/com/banco/domain/responses/TransferenciaResponse.java
@@ -0,0 +1,21 @@
+package br.com.banco.domain.responses;
+
+import java.util.List;
+
+import br.com.banco.domain.entities.Transferencia;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransferenciaResponse {
+
+    private List<Transferencia> transferencias;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
