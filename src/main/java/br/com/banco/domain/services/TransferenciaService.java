435e480e822c52c5696126cbef675dbe01e87828
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-12 14:05:18 -0300

    add lógica para buscar dependendo dos filtros passados

--- src/main/java/br/com/banco/domain/services/TransferenciaService.java
+++ src/main/java/br/com/banco/domain/services/TransferenciaService.java
@@ -1,5 +1,6 @@
 package br.com.banco.domain.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,7 +15,20 @@ public class TransferenciaService {
 
     private final TransferenciaRepository repository;
 
-    public List<Transferencia> findAll() {
-        return repository.findAll();
+    public List<Transferencia> obterTransferencias(LocalDate dataInicial, LocalDate dataFinal, String nomeOperador) {
+
+        if (dataInicial != null && dataFinal != null && nomeOperador == null) {
+            return repository.buscarPorIntervaloDeData(dataInicial, dataFinal.plusDays(1));
+
+        } else if (dataInicial != null && dataFinal != null && nomeOperador != null) {
+            return repository.buscarPorIntervaloDeDataENomeOperador(dataInicial, dataFinal.plusDays(1), nomeOperador);
+
+        } else if (dataInicial == null && dataFinal == null && nomeOperador != null) {
+            return repository.findAllByNomeOperadorTransacaoIgnoreCase(nomeOperador);
+
+        } else {
+            return repository.findAll();
+        }
     }
+
 }
