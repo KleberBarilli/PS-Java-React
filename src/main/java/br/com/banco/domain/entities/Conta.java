b8d2153a7bac04ec6932f5abecb9962bf8d65512
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-12 10:49:52 -0300

    criacao das entidades conta e transferencia

---
+++ src/main/java/br/com/banco/domain/entities/Conta.java
@@ -0,0 +1,29 @@
+package br.com.banco.domain.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "conta")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long idConta;
+
+    @Column(name = "nome_responsavel")
+    private String nomeResponsavel;
+
+}
---
+++ src/main/java/br/com/banco/domain/entities/Transferencia.java
@@ -0,0 +1,43 @@
+package br.com.banco.domain.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transferencia")
+public class Transferencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_transferencia")
+    private LocalDateTime dataTransferencia;
+
+    private BigDecimal valor;
+
+    private String tipo;
+
+    @Column(name = "nome_operador_transacao")
+    private String nomeOperadorTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+}
