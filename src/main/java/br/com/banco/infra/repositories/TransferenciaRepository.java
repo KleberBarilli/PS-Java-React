1d2aaa5dd650e74cee6311ec24b114310b3a2a81
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-12 16:28:51 -0300

    adaptando o repository para ter paginacao

--- src/main/java/br/com/banco/infra/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/infra/repositories/TransferenciaRepository.java
@@ -2,6 +2,8 @@ package br.com.banco.infra.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,12 +14,13 @@ import java.time.LocalDate;
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
 
     @Query("SELECT t FROM Transferencia t WHERE t.dataTransferencia >= :dataInicial AND t.dataTransferencia < :dataFinal")
-    List<Transferencia> buscarPorIntervaloDeData(LocalDate dataInicial, LocalDate dataFinal);
+    Page<Transferencia> buscarPorIntervaloDeData(Pageable pageable, LocalDate dataInicial, LocalDate dataFinal);
 
     @Query("SELECT t FROM Transferencia t WHERE t.dataTransferencia >= :dataInicial AND t.dataTransferencia < :dataFinal AND LOWER(t.nomeOperadorTransacao) = LOWER(:nomeOperador)")
-    List<Transferencia> buscarPorIntervaloDeDataENomeOperador(LocalDate dataInicial, LocalDate dataFinal,
+    Page<Transferencia> buscarPorIntervaloDeDataENomeOperador(Pageable pageable, LocalDate dataInicial,
+            LocalDate dataFinal,
             String nomeOperador);
 
-    List<Transferencia> findAllByNomeOperadorTransacaoIgnoreCase(String nome);
+    Page<Transferencia> findAllByNomeOperadorTransacaoIgnoreCase(Pageable pageable, String nome);
 
 }
